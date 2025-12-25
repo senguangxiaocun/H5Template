848dc73082ee2b8208361971f41209aedee78e3c
SKXIAMO <1831630201@qq.com>
2025-12-25 11:39:10 +0800

    scroll

--- src/pages/other-home/index.vue
+++ src/pages/other-home/index.vue
@@ -1,126 +1,121 @@
 <script setup lang="ts">
-  import { showLoadingToast } from 'vant'
-  import Head from '@/assets/public/Head.png'
-  import { useAppImgStyle } from '@/hooks/useAppImgStyle'
-  import { detailId } from '@/hooks/useDetail'
-  import { useJump } from '@/hooks/useJump'
-  import { useWindow } from '@/hooks/useWindow'
-  import { useUserStore } from '@/stores'
+import { showLoadingToast } from "vant";
+import Head from "@/assets/public/Head.png";
+import { useAppImgStyle } from "@/hooks/useAppImgStyle";
+import { detailId } from "@/hooks/useDetail";
+import { useJump } from "@/hooks/useJump";
+import { useWindow } from "@/hooks/useWindow";
+import { useUserStore } from "@/stores";
 
-  defineOptions({
-    name: 'other-home'
-  })
+defineOptions({
+  name: "other-home",
+});
 
-  const {
-    reportIcon,
-    otherHomeAddIcon,
-    otherHomeMessageIcon,
-    otherHomeLikeIcon
-  } = useAppImgStyle()
-  const { queryId, jumpToDetail, appParams, jumpToPrivateChat } =
-    useJump()
-  const { winUserListData, winDynamicData, winChatListData } = useWindow()
-  const useData = useUserStore()
+const {
+  reportIcon,
+  otherHomeAddIcon,
+  otherHomeMessageIcon,
+  otherHomeLikeIcon,
+} = useAppImgStyle();
+const { queryId, jumpToDetail, appParams, jumpToPrivateChat } = useJump();
+const { winUserListData, winDynamicData, winChatListData } = useWindow();
+const useData = useUserStore();
 
-  // 举报弹框
-  const isReport = ref(false)
-  const userInfo = ref<UserInfo>(null)
-  const bottomList = ref<DynamicInfo[]>([])
-  const loading = ref(true)
-  /** 是否显示关注 */
-  const isShowFollow = ref(false)
-  const allUserList = ref<UserInfo[]>(winUserListData)
+// 举报弹框
+const isReport = ref(false);
+const userInfo = ref<UserInfo>(null);
+const bottomList = ref<DynamicInfo[]>([]);
+const loading = ref(true);
+/** 是否显示关注 */
+const isShowFollow = ref(false);
+const allUserList = ref<UserInfo[]>(winUserListData);
 
-  const getData = () => {
-    userInfo.value = winUserListData.find(v => v.userId === queryId.value)
+const getData = () => {
+  userInfo.value = winUserListData.find((v) => v.userId === queryId.value);
 
-    detailId.value = userInfo.value.userId
+  detailId.value = userInfo.value.userId;
 
-    bottomList.value = winDynamicData.filter(
-      v => v.userId === userInfo.value.userId
-    )
-    console.log(bottomList.value, '====')
-    isShowFollow.value = useData.userInfo.follow.includes(
-      userInfo.value.userId
-    )
-    loading.value = false
-  }
+  bottomList.value = winDynamicData.filter((v) => v.userId === userInfo.value.userId);
+  console.log(bottomList.value, "====");
+  isShowFollow.value = useData.userInfo.follow.includes(userInfo.value.userId);
+  loading.value = false;
+};
 
-  const onGoDetail = (item: DynamicInfo) => {
-    jumpToDetail(item.dynamicId, item.dynamicType, queryId.value)
-  }
+const onGoDetail = (item: DynamicInfo) => {
+  jumpToDetail(item.dynamicId, item.dynamicType, queryId.value);
+};
 
-  const onFollow = () => {
-    useData.userInfo.follow.push(userInfo.value.userId)
-    userInfo.value.fans.push(useData.userInfo.userId)
-    allUserList.value.forEach(v => {
-      if (v.userId === useData.userInfo.userId) {
-        v.follow = useData.userInfo.follow
-      }
-      if (v.userId === userInfo.value.userId) {
-        v.fans = userInfo.value.fans
-      }
-    })
-    isShowFollow.value = true
-    appParams({ key: 'updateUser', value: allUserList.value, state: 1 })
-  }
-
-  const getCurrentDateTime = (): string => {
-    const now = new Date()
-    const year = now.getFullYear()
-    const month = String(now.getMonth() + 1).padStart(2, '0') // getMonth() 是 0-11
-    const day = String(now.getDate()).padStart(2, '0')
-    const hours = String(now.getHours()).padStart(2, '0')
-    const minutes = String(now.getMinutes()).padStart(2, '0')
-    const seconds = String(now.getSeconds()).padStart(2, '0')
-
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-  }
-
-  const onAddChat = () => {
-    const chatItem = winChatListData.find(v => {
-      return (
-        v.chatUserIds.includes(userInfo.value.userId) &&
-        v.chatUserIds.includes(useData.userInfo.userId)
-      )
-    })
-    if (chatItem) {
-      jumpToPrivateChat(chatItem.chatId, queryId.value)
-    } else {
-      // 时间戳
-      const item: ChatInfo = {
-        chatId: `${Date.now()}_chatId`,
-        chatUserIds: [userInfo.value.userId, useData.userInfo.userId],
-        lastSendContent: '',
-        lastSendTime: getCurrentDateTime(),
-        unreadMsgCount: 0,
-        lastSendUserId: useData.userInfo.userId
-      }
-      window?.chatListJson.push(item)
-
-      showLoadingToast({
-        message: 'Loading...',
-        forbidClick: true,
-        onClose: () => {
-          appParams({
-            key: 'uploadChat',
-            value: window?.chatListJson,
-            state: 1
-          })
-          jumpToPrivateChat(item.chatId, queryId.value)
-        }
-      })
+const onFollow = () => {
+  useData.userInfo.follow.push(userInfo.value.userId);
+  userInfo.value.fans.push(useData.userInfo.userId);
+  allUserList.value.forEach((v) => {
+    if (v.userId === useData.userInfo.userId) {
+      v.follow = useData.userInfo.follow;
     }
-  }
+    if (v.userId === userInfo.value.userId) {
+      v.fans = userInfo.value.fans;
+    }
+  });
+  isShowFollow.value = true;
+  appParams({ key: "updateUser", value: allUserList.value, state: 1 });
+};
 
-  onMounted(() => {
-    getData()
-  })
+const getCurrentDateTime = (): string => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0"); // getMonth() 是 0-11
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
 
-  const shouldShowReport = (item) => {
-    // 不显示自己
-    return item.userId !== useData.userInfo.userId
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
+const onAddChat = () => {
+  const chatItem = winChatListData.find((v) => {
+    return (
+      v.chatUserIds.includes(userInfo.value.userId) &&
+      v.chatUserIds.includes(useData.userInfo.userId)
+    );
+  });
+  if (chatItem) {
+    jumpToPrivateChat(chatItem.chatId, queryId.value);
+  } else {
+    // 时间戳
+    const item: ChatInfo = {
+      chatId: `${Date.now()}_chatId`,
+      chatUserIds: [userInfo.value.userId, useData.userInfo.userId],
+      lastSendContent: "",
+      lastSendTime: getCurrentDateTime(),
+      unreadMsgCount: 0,
+      lastSendUserId: useData.userInfo.userId,
+    };
+    window?.chatListJson.push(item);
+
+    showLoadingToast({
+      message: "Loading...",
+      forbidClick: true,
+      onClose: () => {
+        appParams({
+          key: "uploadChat",
+          value: window?.chatListJson,
+          state: 1,
+        });
+        jumpToPrivateChat(item.chatId, queryId.value);
+      },
+    });
   }
+};
+
+onMounted(() => {
+  getData();
+});
+
+const shouldShowReport = (item) => {
+  // 不显示自己
+  return item.userId !== useData.userInfo.userId;
+};
 </script>
 
 <template>
@@ -146,7 +141,7 @@
             @click="onFollow"
             :style="{
               width: 'var(--other-home-follow-width)',
-              height: 'var(--other-home-follow-height)'
+              height: 'var(--other-home-follow-height)',
             }"
           />
         </div>
@@ -168,17 +163,16 @@
       </ul>
       <ul px-layout-padding class="bottom-box">
         <li>{{ userInfo.about }}</li>
-        <li
-          v-if="shouldShowReport(userInfo)"
-        >
-          <van-image :src="otherHomeMessageIcon" class="icon-box" 
+        <li v-if="shouldShowReport(userInfo)">
+          <van-image
+            :src="otherHomeMessageIcon"
+            class="icon-box"
             :style="{
               width: 'var(--other-home-chat-width)',
-              height: 'var(--other-home-chat-height)'
-            }"/>
-          <span ml-3 class="public-number !mt-0" @click="onAddChat">
-            Chat
-          </span>
+              height: 'var(--other-home-chat-height)',
+            }"
+          />
+          <span ml-3 class="public-number !mt-0" @click="onAddChat"> Chat </span>
         </li>
       </ul>
     </div>
@@ -200,16 +194,16 @@
           <li />
           <li>
             <van-image
-              v-if="shouldShowReport(item)" 
+              v-if="shouldShowReport(item)"
               :src="reportIcon"
               :style="{
                 width: 'var(--report-image-width)',
-                height: 'var(--report-image-height)'
+                height: 'var(--report-image-height)',
               }"
               @click.stop="
                 () => {
-                  isReport = true
-                  detailId = item.userId
+                  isReport = true;
+                  detailId = item.userId;
                 }
               "
             />
@@ -234,10 +228,14 @@
         </ul>
         <span class="bottom-text">{{ item.dynamicDesc }}</span>
         <div class="like-box">
-          <van-image :src="otherHomeLikeIcon" class="icon-box" :style="{
+          <van-image
+            :src="otherHomeLikeIcon"
+            class="icon-box"
+            :style="{
               width: 'var(--other-home-like-post-width)',
-              height: 'var(--other-home-like-post-height)'
-            }"/>
+              height: 'var(--other-home-like-post-height)',
+            }"
+          />
           <span class="public-number">{{ item.dynamicLikeCount }}</span>
         </div>
       </div>
@@ -248,150 +246,160 @@
 </template>
 
 <style lang="less" scoped>
-  .other-home_box {
-    min-height: 100vh;
-    background: var(--ai-other-home-bg-color);
-  } 
+.other-home_box {
+  height: 100%;
+  overflow-y: auto;
+  overscroll-behavior: contain;
+  -webkit-overflow-scrolling: touch;
+  background: var(--ai-other-home-bg-color);
+}
+html,
+body {
+  height: 100%;
+  overflow: hidden;
+  overscroll-behavior: none;
+}
 
-  .top-user-info {
-    background: url('@/assets/public/top-home.png');
-    background-size: cover;
-    width: 100%;
-    height: 346px;
+body {
+  margin: 0;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  -webkit-overflow-scrolling: auto;
+}
+.top-user-info {
+  background: url("@/assets/public/top-home.png");
+  background-size: cover;
+  width: 100%;
+  height: 346px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .bottom-box {
     display: flex;
-    flex-direction: column;
     justify-content: space-between;
-
-    .bottom-box {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-
-      li {
-        color: var(--ai-other-home-right-desc-text-color);
-        font-size: var(--ai-other-home-right-desc-text-weight);
-        font-weight: var(--ai-other-home-right-desc-text-size);
-
-        &:nth-child(2) {
-          margin-left: 10px;
-          width: var(--ai-other-home-right-btn-style-width);
-          height: var(--ai-other-home-right-btn-style-height);
-          flex-shrink: 0;
-          border-radius: var(
-            --ai-other-home-right-btn-style-border-radius
-          );
-          background: var(--ai-other-home-right-btn-style-bg-color);
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-      }
-    }
-
-    .number-box {
-      margin: 6px 0;
-
-      li {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-
-        span {
-          font-size: var(--ai-other-home-top-data-desc-text-size);
-          font-weight: var(--ai-other-home-top-data-desc-text-weight);
-          color: var(--ai-other-home-top-data-desc-text-color);
-
-          &:nth-child(2) {
-            font-size: var(--ai-other-home-top-data-text-text-size);
-            font-weight: var(--ai-other-home-top-data-text-text-weight);
-            color: var(--ai-other-home-top-data-text-text-color);
-          }
-        }
-      }
-    }
-  }
-
-  .avatar-info {
-    padding: 96px 0 0;
-    display: flex;
-    flex-direction: column;
     align-items: center;
 
-    .user-head {
-      width: 83px;
-      height: 83px;
-      border: 3px solid #fff;
+    li {
+      color: var(--ai-other-home-right-desc-text-color);
+      font-size: var(--ai-other-home-right-desc-text-weight);
+      font-weight: var(--ai-other-home-right-desc-text-size);
+
+      &:nth-child(2) {
+        margin-left: 10px;
+        width: var(--ai-other-home-right-btn-style-width);
+        height: var(--ai-other-home-right-btn-style-height);
+        flex-shrink: 0;
+        border-radius: var(--ai-other-home-right-btn-style-border-radius);
+        background: var(--ai-other-home-right-btn-style-bg-color);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
     }
   }
 
-  .bottom-card {
-    .card-item + .card-item {
-      margin-top: 20px;
+  .number-box {
+    margin: 6px 0;
+
+    li {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      span {
+        font-size: var(--ai-other-home-top-data-desc-text-size);
+        font-weight: var(--ai-other-home-top-data-desc-text-weight);
+        color: var(--ai-other-home-top-data-desc-text-color);
+
+        &:nth-child(2) {
+          font-size: var(--ai-other-home-top-data-text-text-size);
+          font-weight: var(--ai-other-home-top-data-text-text-weight);
+          color: var(--ai-other-home-top-data-text-text-color);
+        }
+      }
     }
+  }
+}
 
-    .card-item {
-      background: rgba(40, 35, 41, 0.8);
-      border-radius: 20px;
-      position: relative;
-      overflow: hidden;
+.avatar-info {
+  padding: 96px 0 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-      .top-info {
-        padding: 16px;
+  .user-head {
+    width: 83px;
+    height: 83px;
+    border: 3px solid #fff;
+  }
+}
+
+.bottom-card {
+  .card-item + .card-item {
+    margin-top: 20px;
+  }
+
+  .card-item {
+    background: rgba(40, 35, 41, 0.8);
+    border-radius: 20px;
+    position: relative;
+    overflow: hidden;
+
+    .top-info {
+      padding: 16px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      li {
         display: flex;
         align-items: center;
-        justify-content: space-between;
 
-        li {
-          display: flex;
-          align-items: center;
-
-          .tag {
-            background: rgba(255, 255, 255, 0.2);
-          }
+        .tag {
+          background: rgba(255, 255, 255, 0.2);
         }
-      }
-
-      .bottom-img {
-        padding: 0 16px 16px;
-        display: flex;
-        justify-content: space-between;
-
-        li {
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-        }
-      }
-
-      .bottom-text {
-        position: absolute;
-        bottom: 0;
-        background: linear-gradient(
-          90deg,
-          #0e080f78 0%,
-          rgba(14, 8, 15, 0) 100%
-        );
-        height: 42px;
-        width: 100%;
-        line-height: 42px;
-        padding: 0 16px;
-        font-size: var(--ai-other-home-card-desc-text-size);
-        font-weight: var(--ai-other-home-card-desc-text-weight);
-        color: var(--ai-other-home-card-desc-text-color);
-        text-align: var(--ai-other-home-card-desc-text-sort);
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-      }
-
-      .like-box {
-        position: absolute;
-        bottom: 26px;
-        right: 10px;
-        display: flex;
-        flex-direction: column;
-        align-self: center;
       }
     }
+
+    .bottom-img {
+      padding: 0 16px 16px;
+      display: flex;
+      justify-content: space-between;
+
+      li {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+    }
+
+    .bottom-text {
+      position: absolute;
+      bottom: 0;
+      background: linear-gradient(90deg, #0e080f78 0%, rgba(14, 8, 15, 0) 100%);
+      height: 42px;
+      width: 100%;
+      line-height: 42px;
+      padding: 0 16px;
+      font-size: var(--ai-other-home-card-desc-text-size);
+      font-weight: var(--ai-other-home-card-desc-text-weight);
+      color: var(--ai-other-home-card-desc-text-color);
+      text-align: var(--ai-other-home-card-desc-text-sort);
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    .like-box {
+      position: absolute;
+      bottom: 26px;
+      right: 10px;
+      display: flex;
+      flex-direction: column;
+      align-self: center;
+    }
   }
+}
 </style>
