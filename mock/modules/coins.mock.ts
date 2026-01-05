ba749a144b75fd9d1b143f565e84c0d48fa7f47d
SKXIAMO <1831630201@qq.com>
2026-01-05 14:54:44 +0800

    4

--- mock/modules/coins.mock.ts
+++ mock/modules/coins.mock.ts
@@ -1,53 +1,53 @@
 // 金币选项数据
 export default [
   {
-    key: 'lvbsvhxcgcrvesor',
+    key: 'zaqlfjnhvlzguium',
     cions: 400,
     meney: 0.99
   },
   {
-    key: 'lvbsvhcgcrvesor',
+    key: 'zvflakgvtbdvmzud',
     cions: 800,
     meney: 1.99
   },
   {
-    key: '111',
+    key: 'bvnc4mx9zlaksj7d',
     cions: 2190,
     meney: 3.99
   },
   {
-    key: 'dxismgcwewhrtezo',
+    key: 'vpcrbkmkqkghnmep',
     cions: 2450,
     meney: 4.99
   },
   {
-    key: '555',
+    key: 'osgfyruejd2bxkal',
     cions: 3950,
     meney: 8.99
   },
   {
-    key: 'khtxlcejaxmqcsra',
-    cions: 4900,
+    key: 'byqaaxioghmbtmgr',
+    cions: 5150,
     meney: 9.99
   },
   {
-    key: '6666',
+    key: 'qndufb7soakz0kcd',
     cions: 7900,
     meney: 14.99
   },
   {
-    key: 'yadwwvxspgxwlndb',
-    cions: 9800,
+    key: 'zycqlyqtstutpncp',
+    cions: 10800,
     meney: 19.99
   },
   {
-    key: 'qnrcuelbtiuflyky',
-    cions: 24500,
+    key: 'xegcsebldyfxwkum',
+    cions: 29400,
     meney: 49.99
   },
   {
-    key: 'ymohxnvpkqxutvab',
-    cions: 49000,
+    key: 'qhnsipzqhruqmlxl',
+    cions: 63700,
     meney: 99.99
   }
 ] as CoinInfo[]
